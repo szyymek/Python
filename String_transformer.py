5bd15178949cd2f6b25e43a0562c348512808719
szyymek <45272766+szyymek@users.noreply.github.com>
2019-09-11 10:23:18 +0200

    Create String_transformer.py

---
+++ String_transformer.py
@@ -0,0 +1,17 @@
+def string_transformer(s):
+    s = s.split(" ")
+    s = s[::-1]
+    print(s)
+    s = " ".join(s)
+    print(s)
+    result = ""
+
+    for letter in s:
+        if letter.islower():
+            result += letter.upper()
+        elif letter.isupper():
+            result += letter.lower()
+        else:
+            result +=letter
+    
+    return result
