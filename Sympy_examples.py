9823bed9438c5df3824727c6edeb6779a9d634a0
szyymek <45272766+szyymek@users.noreply.github.com>
2019-09-11 12:19:39 +0200

    Update Sympy_examples.py

--- Sympy_examples.py
+++ Sympy_examples.py
@@ -1,6 +1,6 @@
 from sympy import *
 from sympy.plotting import plot, plot_parametric
 
-expr = [sin(2*sin(x**3)), sin(x), cos(x)]
+expr = [sin(2*sin(x**3)), sin(x), cos(x), x**2]
 for funkcja in expr:
     plot(funkcja, (x,0,5))
