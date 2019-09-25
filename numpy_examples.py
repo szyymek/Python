b47ce0b2c8df9efe408c202e8aa989230f374ce9
szyymek <45272766+szyymek@users.noreply.github.com>
2019-09-25 17:14:00 +0200

    Update numpy_examples.py

--- numpy_examples.py
+++ numpy_examples.py
@@ -15,16 +15,23 @@ sinusy = [np.sin(i) for i in D] #count sinus for 100 numbers between 0 and 2pi
 #print(D)
 #print(sinusy)
 
-wynik = np.linalg.solve(A,B) #solve linearequation
-
-#print(wynik)
-
 E = np.arange(12).reshape(3,4)
 #print(E)
 #print(E[:,[1,0,2,3]]) #swap column 1 and 2 in matrix
 #print(E[[1,0,2], :]) #swap rows 1 and 2 in matrix
 #print(E[::-1]) # reverse rows in matrix
 
-
 F = np.random.randint(5, 11, size=(5,3)) #print matrix(5,3) of random integers between 5 and 10
 #print(F)
+
+norma = np.linalg.norm(F) #calculate matrix norm
+#print(norma)
+
+wyznacznik = np.linalg.det(A) #calculate matrix determinant
+#print(wyznacznik)
+
+iloczyn_skalarny = np.dot(A,B) # calculate dot product of arrays
+#print(iloczyn_skalarny)
+
+rownanie_algebraiczne = np.linalg.solve(A,B) #solve linearequation
+#print(rownanie_algebraiczne)
