ea8788b2023763c2e64c9b1b4a034492247720be
szyymek <45272766+szyymek@users.noreply.github.com>
2019-09-26 21:04:36 +0200

    Update lists.py

--- lists.py
+++ lists.py
@@ -11,3 +11,23 @@ print(lista_imion)
 #Loop through the list
 for i in lista_imion:
     print(i)
+    
+#add element to list
+lista_imion.append('Marian')
+print(lista_imion)
+
+#add element to list on specific place
+lista_imion.insert(2,'Wojciech')
+print(lista_imion)
+
+#sort list
+lista_imion.sort()
+print(lista_imion)
+
+#remove specific list element
+lista_imion.pop(3)
+print(lista_imion)
+
+#clear all elements of list
+lista_imion.clear()
+print(lista_imion)
