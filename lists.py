7977cc4a0150101855275694b4f23a969c12e339
szyymek <45272766+szyymek@users.noreply.github.com>
2019-09-26 17:10:35 +0200

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,13 @@
+#List is a collection which is ordered and changeable. Allows duplicate members.
+lista_imion = ['Jan', 'Piotr', 'Marcin', 'Tomasz', 'Grzegorz']
+
+#print range of list
+print(lista_imion[2:4])
+
+#change element of list
+lista_imion[1] = 'Janusz'
+print(lista_imion)
+
+#Loop through the list
+for i in lista_imion:
+    print(i)
